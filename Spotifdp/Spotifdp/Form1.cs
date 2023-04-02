5cb1c2e38a8385558ad3db80d269bbf9692550e2
Baptu_ <113063172+Baptu54@users.noreply.github.com>
2023-04-02 15:48:02 +0200

    Add files via upload

---
+++ Spotifdp/Spotifdp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Spotifdp
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Process.Start("curl", "parrot.live");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            Process.Start("powershell.exe", "iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex\r\niwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex");            
+            MessageBox.Show("Veuillez Patienter et ne pas fermer Windows Powershell");
+            Environment.Exit(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Spotifdp/Spotifdp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Net.Http;
+using Microsoft.Win32;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Net;
+using System.Globalization;
+using System.Timers;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+
+namespace Spotifdp
+{
+    public partial class Program
+    {
+        
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            registryKey.SetValue("Spotifdp", Application.ExecutablePath);
+            for (; ; )
+            {
+                if (Process.GetProcessesByName("spotify").Length > 0)
+                {
+                    Application.Run(new Form1());
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(5000);
+                }                
+            }
+        }      
+    }
+}
